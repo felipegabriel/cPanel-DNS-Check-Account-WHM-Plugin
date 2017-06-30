6d205f4d28c05ad2a23598c63766c4537edf25af
codebr <felipegabriel@users.noreply.github.com>
2017-06-30 20:16:58 -0300

    Add files via upload

---
+++ cron.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * /usr/local/cpanel/3rdparty/bin/php -q /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-account-dns-check/cron.php
+ */
+$hostname = gethostname();
+ob_start();
+include('dns-check.php');
+$value = ob_get_contents();
+ob_end_clean();
+$to = 'root@' . $hostname;
+$subject = 'Panel DNS Check Account WHM Plugin';
+$message = $value;
+$headers = 'From: root@' . $hostname . "\r\n" .
+        'X-Mailer: PHP/' . phpversion() . "\r\n" .
+        'Content-Type: text/html; charset=ISO-8859-1\r\n';
+
+mail($to, $subject, $message, $headers);
---
+++ dns-check.php
@@ -0,0 +1,123 @@
+<?php
+
+function resolve_domain($domain) {
+    $dns = '8.8.8.8';  // Google Public DNS
+    if (rand(0, 1) == 1) {
+        $dns = '208.67.222.222'; // Open DNS
+    }
+    $ip = `nslookup $domain $dns`; // the backticks execute the command in the shell
+    $ips = array();
+    if (preg_match_all('/Address: ((?:\d{1,3}\.){3}\d{1,3})/', $ip, $match) > 0) {
+        $ips = $match[1];
+    }
+    return $ips;
+}
+
+function open_file_per_line($file) {
+    $handle = fopen($file, "r");
+    if ($handle) {
+        $lines = array();
+        while (($line = fgets($handle)) !== false) {
+            $lines[] = trim($line);
+        }
+        return $lines;
+        fclose($handle);
+    } else {
+        return false;
+    }
+}
+
+function check_valid_resolve_ip($ip, $domain) {
+    if ($domain == '_SERVER_HOSTNAME_') {
+        return array('label' => 'info', 'msg' => '');
+    }
+    if (filter_var($ip, FILTER_VALIDATE_IP) == false) {
+        return array('label' => 'danger', 'msg' => 'Invalid IP');
+    }
+    $domain_local_ip = get_domain_ip_local_file($domain);
+    if ($domain_local_ip['ip'] != $ip) {
+        return array('label' => 'danger', 'msg' => 'Different IP');
+    }
+    return array('label' => 'success', 'msg' => '');
+}
+
+function get_domain_ip_local_file($domain) {
+    $file_lines = open_file_per_line('/etc/userdatadomains');
+    foreach ($file_lines as $line) {
+        $explode = explode('==', $line);
+        $explode_two = explode(':', $explode[0]);
+        if (trim($explode_two[0]) == trim($domain)) {
+            $ip_port = $explode[5];
+            $explode_ip = explode(':', $ip_port);
+            return array('ip' => $explode_ip[0], 'acc' => trim($explode_two[1]));
+        }
+    }
+}
+
+$all_domains_local = open_file_per_line('/etc/localdomains');
+$hostname = gethostname();
+?>
+<html>
+    <head>
+        <title>DNS Check Account</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>cPanel DNS Check Account WHM Plugin</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <td>User</td>
+                                <td>Domain</td>
+                                <td>Local IP</td>
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            foreach ($all_domains_local as $domain) {
+                                $domain_local_acc = get_domain_ip_local_file($domain);
+                                $resolve_ips = resolve_domain($domain);
+                                $ips_ = '';
+                                foreach ($resolve_ips as $ip) {
+                                    if ($domain == $hostname) {
+                                        $domain = '_SERVER_HOSTNAME_';
+                                    }
+                                    $check = check_valid_resolve_ip($ip, $domain);
+                                    $ips_ .= '<span class="label label-' . $check['label'] . '">' . $ip . '</span> ' . $check['msg'] . '<br><br>';
+                                }
+                                $ips = rtrim($ips_, '<br>');
+                                $ip_result_html = $ips != '' ? $ips : '<span class="label label-danger">Not Resolve</span>';
+                                if ($domain == '_SERVER_HOSTNAME_') {
+                                    $domain = $hostname;
+                                    $domain_local_acc['acc'] = 'root';
+                                }
+                                ?>
+                                <tr>
+                                    <td><?= $domain_local_acc['acc'] ?></td>
+                                    <td><?= $domain ?></td>
+                                    <td><?= $domain_local_acc['ip'] ?></td>
+                                    <td><?= $ip_result_html ?><br></td>
+                                </tr>
+                                <?php
+                                $i++;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
